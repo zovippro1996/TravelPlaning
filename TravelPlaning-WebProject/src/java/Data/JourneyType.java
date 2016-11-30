f802546e829d1a72b2c86f1bae1d16c5ca11903a
Krankai <bay25796@gmail.com>
2016-11-30 23:16:25 +0700

    Update servlet to generate journey; change header and footer file

---
+++ TravelPlaning-WebProject/src/java/Data/JourneyType.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+import java.util.*;
+
+/**
+ *
+ * @author krankai
+ */
+public enum JourneyType
+{
+    SIGHTSEEING(new LocationType[] {LocationType.CULTURAL, LocationType.LANDMARK,
+        LocationType.ARTISTIC }),
+    METROPOLIS(new LocationType[] {LocationType.LANDMARK, LocationType.SCIENTIFIC,
+        LocationType.INDUSTRIAL, LocationType.ARTISTIC }),
+    EDUCATION(new LocationType[] {LocationType.HISTORICAL, LocationType.SCIENTIFIC,
+        LocationType.ARTISTIC, LocationType.CULTURAL });
+    
+    LocationType[] listLocations;
+    
+    JourneyType(LocationType[] list)
+    {
+        listLocations = list;
+    }
+    
+    public String getListLocations()
+    {
+        String result = "(";
+        for (int i = 0; i < listLocations.length; ++i)
+        {
+            if (i != 0)
+            {
+                result += ", ";
+            }
+            result += listLocations[i].name();
+        }
+        result += ")";
+        
+        return result;
+    }
+}
