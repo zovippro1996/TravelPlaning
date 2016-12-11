2551c9571da4b143c15137a7dc7a5ad60881ee62
Nguyễn Lương Huy <forcer1996@gmail.com>
2016-12-11 22:44:17 +0700

    Create class Comment

---
+++ TravelPlaning-WebProject/src/java/Data/Comment.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+/**
+ *
+ * @author CREAT10N
+ */
+public class Comment{
+
+    private String description;
+    private double rate;
+
+    public Comment() {
+    }
+
+    public Comment(String description, double rate) {
+        this.description = description;
+        this.rate = rate;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the rate
+     */
+    public double getRate() {
+        return rate;
+    }
+
+    /**
+     * @param rate the rate to set
+     */
+    public void setRate(double rate) {
+        this.rate = rate;
+    }    
+}
